e8b87bf5b3fae4108c4edb8ced398f48f1f07e04
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2025-03-23 09:39:43 +0900

    Update detect-language.sh

--- src/scripts/detect-language.sh
+++ src/scripts/detect-language.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 언어 감지 (기본: 영어)
-LANG_CODE=$(locale 2>/dev/null | grep -E '^LANG=' | cut -d= -f2 | cut -d_ -f1)
+LANG_CODE=$(locale 2>/dev/null | grep -E '^LANG=' | cut -d= -f2 | cut -d_ -f1 | sed 's/\"//g')
 LANG_CODE=${LANG_CODE:-en}
 
 LANG_FILE="$HOME/.direnv/src/lang/${LANG_CODE}.lang"
